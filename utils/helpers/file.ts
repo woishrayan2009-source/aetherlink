2ac72a9ea78fd40240655ab9bfec16d62dc7fb20
Ankush <deshwalankush23@gmail.com>
2025-11-12 23:07:18 +0530

    slight change

--- utils/helpers/file.ts
+++ utils/helpers/file.ts
@@ -2,16 +2,6 @@ import { NetworkProfile } from '@/types/NetworkProfile';
 import axios from 'axios';
 const API_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:8080";
 
-export
-    const getStatusColor = () => {
-        switch (currentProfile.color) {
-            case "green": return isDark ? "text-green-400 bg-green-500/20 border-green-500/30" : "text-green-600 bg-green-100 border-green-300";
-            case "yellow": return isDark ? "text-yellow-400 bg-yellow-500/20 border-yellow-500/30" : "text-yellow-600 bg-yellow-100 border-yellow-300";
-            case "red": return isDark ? "text-red-400 bg-red-500/20 border-red-500/30" : "text-red-600 bg-red-100 border-red-300";
-            default: return isDark ? "text-cyan-400 bg-cyan-500/20 border-cyan-500/30" : "text-cyan-600 bg-cyan-100 border-cyan-300";
-        }
-    };
-
 export const bufferToHex = (buf: ArrayBuffer) => {
     const arr = new Uint8Array(buf);
     return Array.from(arr)
